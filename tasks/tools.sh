d07ac43dfd4a216831901d29407fc6c3df6750ee
gruberdev <contact@gruber.dev.br>
2022-08-21 22:10:17 -0300

    reverted last commit moved files

---
+++ tasks/tools.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d -b ~/.local/bin
+wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash && sudo mv kustomize /usr/local/bin
