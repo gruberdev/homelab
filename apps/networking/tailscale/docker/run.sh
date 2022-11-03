3ba16d21ec1579ad8d4b1a4108d7f8cbcb6b5a28
gruberdev <contact@gruber.dev.br>
2022-11-03 06:44:14 -0300

    [tailscale] Upgraded & added new ipv6 forwarding

---
+++ apps/networking/tailscale/docker/run.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+# Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+# from https://raw.githubusercontent.com/tailscale/tailscale/main/docs/k8s/run.sh
+
+export PATH="$PATH:/tailscale/bin"
+
+AUTH_KEY="${AUTH_KEY:-}"
+TS_ROUTES="${ROUTES:-}"
+TS_DEST_IP="${DEST_IP:-}"
+DEST_PORT="${DEST_PORT:-}"
+TS_ACCEPT_DNS="${TS_ACCEPT_DNS:-false}"
+TS_EXTRA_ARGS="${EXTRA_ARGS:-}"
+TS_USERSPACE="${USERSPACE:-true}"
+TS_KUBE_SECRET="${KUBE_SECRET:-tailscale}"
+HOSTNAME="${HOSTNAME:-}"
+TS_SOCKET="${TS_SOCKET:-/tmp/tailscaled.sock}"
+
+set -e
+
+chmod +x /iptables.sh
+sh /iptables.sh
+
+TAILSCALED_ARGS="--socket=${TS_SOCKET}"
+
+if [[ ! -z "${KUBERNETES_SERVICE_HOST}" ]]; then
+  TAILSCALED_ARGS="${TAILSCALED_ARGS} --state=kube:${TS_KUBE_SECRET} --statedir=${TS_STATE_DIR:-/tmp}"
+elif [[ ! -z "${TS_STATE_DIR}" ]]; then
+  TAILSCALED_ARGS="${TAILSCALED_ARGS} --statedir=${TS_STATE_DIR}"
+else
+  TAILSCALED_ARGS="${TAILSCALED_ARGS} --state=mem: --statedir=/tmp"
+fi
+
+if [[ "${TS_USERSPACE}" == "true" ]]; then
+  if [[ ! -z "${TS_DEST_IP}" ]]; then
+    echo "IP forwarding is not supported in userspace mode"
+    exit 1
+  fi
+  TAILSCALED_ARGS="${TAILSCALED_ARGS} --tun=userspace-networking"
+else
+  if [[ ! -d /dev/net ]]; then
+    mkdir -p /dev/net
+  fi
+
+  if [[ ! -c /dev/net/tun ]]; then
+    mknod /dev/net/tun c 10 200
+  fi
+fi
+
+if [[ ! -z "${TS_TAILSCALED_EXTRA_ARGS}" ]]; then
+  TAILSCALED_ARGS="${TAILSCALED_ARGS} ${TS_TAILSCALED_EXTRA_ARGS}"
+fi
+
+handler() {
+  echo "Caught SIGINT/SIGTERM, shutting down tailscaled"
+  kill -s SIGINT $PID
+  wait ${PID}
+}
+
+echo "Starting tailscaled"
+tailscaled ${TAILSCALED_ARGS} &
+PID=$!
+trap handler SIGINT SIGTERM
+
+UP_ARGS="--accept-dns=${TS_ACCEPT_DNS}"
+if [[ ! -z "${TS_ROUTES}" ]]; then
+  UP_ARGS="--advertise-routes=${TS_ROUTES} ${UP_ARGS}"
+fi
+if [[ ! -z "${AUTH_KEY}" ]]; then
+  UP_ARGS="--authkey=${AUTH_KEY} ${UP_ARGS}"
+fi
+if [[ ! -z "${TS_EXTRA_ARGS}" ]]; then
+  UP_ARGS="${UP_ARGS} ${TS_EXTRA_ARGS:-}"
+fi
+
+echo "Running tailscale up"
+tailscale --socket=/tmp/tailscaled.sock up ${UP_ARGS}
+
+if [[ ! -z "${TS_DEST_IP}" ]]; then
+  echo "Adding iptables rule for DNAT"
+  iptables -t nat -I PREROUTING -d "$(tailscale --socket=${TS_SOCKET} ip -4)" -j DNAT --to-destination "${DEST_IP}"
+  ip6tables -t nat -I PREROUTING -d "$(tailscale --socket=${TS_SOCKET} ip -6)" -j DNAT --to-destination "${DEST_IP}"
+fi
+
+if [[ ! -z "${DEST_PORT}" ]]; then
+  echo "Adding iptables rules for REDIRECT"
+  iptables -t nat -I PREROUTING -p tcp -d "$(tailscale --socket=${TS_SOCKET} ip -4)" --dport 1:65535 -j REDIRECT --to-ports "${DEST_PORT}"
+  ip6tables -t nat -I PREROUTING -p tcp -d "$(tailscale --socket=${TS_SOCKET} ip -6)" --dport 1:65535 -j REDIRECT --to-ports "${DEST_PORT}"
+fi
+
+echo "Waiting for tailscaled to exit"
+wait ${PID}
