793f1dd4df7f7cd88a8ee0be61c866dbf04bb2d7
gruberdev <contact@gruber.dev.br>
2023-01-17 19:14:56 -0300

    [tailscale] fixed permission error

--- apps/networking/tailscale/docker/run.sh
+++ apps/networking/tailscale/docker/run.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-# Copyright (c) 2021 Tailscale Inc & AUTHORS All rights reserved.
-# Use of this source code is governed by a BSD-style
-# license that can be found in the LICENSE file.
 
 # from https://raw.githubusercontent.com/tailscale/tailscale/main/docs/k8s/run.sh
 
